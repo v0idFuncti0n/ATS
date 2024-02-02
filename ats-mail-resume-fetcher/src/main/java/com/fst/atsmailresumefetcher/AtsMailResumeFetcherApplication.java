3a8bc1071f5c0c113e294bc078d4bb82298eda0b
Anas <lakchouchanas@gmail.com>
2024-02-02 15:31:14 +0100

    add ats-mail-resume-fetcher module

---
+++ ats-mail-resume-fetcher/src/main/java/com/fst/atsmailresumefetcher/AtsMailResumeFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.fst.atsmailresumefetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsMailResumeFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsMailResumeFetcherApplication.class, args);
+    }
+
+}
---
+++ ats-mail-resume-fetcher/src/test/java/com/fst/atsmailresumefetcher/AtsMailResumeFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.atsmailresumefetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsMailResumeFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
