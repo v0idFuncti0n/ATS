d316d26af8f898c3fe6f6ee3fc33c6e7cd683adb
Anas <lakchouchanas@gmail.com>
2024-02-02 15:03:31 +0100

    add the configuration for ats-configuration-server-service

--- ats-configuration-service-server/src/main/java/com/fst/atsconfigurationserviceserver/AtsConfigurationServiceServerApplication.java
+++ ats-configuration-service-server/src/main/java/com/fst/atsconfigurationserviceserver/AtsConfigurationServiceServerApplication.java
@@ -2,8 +2,10 @@ package com.fst.atsconfigurationserviceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AtsConfigurationServiceServerApplication {
 
     public static void main(String[] args) {
