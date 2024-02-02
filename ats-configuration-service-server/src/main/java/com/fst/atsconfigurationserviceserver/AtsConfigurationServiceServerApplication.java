be05f79902dafe9f33c23d34a41f515b353319b6
Anas <lakchouchanas@gmail.com>
2024-02-02 14:47:52 +0100

    add ats-configuration-server-service module

---
+++ ats-configuration-service-server/src/main/java/com/fst/atsconfigurationserviceserver/AtsConfigurationServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.fst.atsconfigurationserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsConfigurationServiceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsConfigurationServiceServerApplication.class, args);
+    }
+
+}
---
+++ ats-configuration-service-server/src/test/java/com/fst/atsconfigurationserviceserver/AtsConfigurationServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.atsconfigurationserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsConfigurationServiceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
