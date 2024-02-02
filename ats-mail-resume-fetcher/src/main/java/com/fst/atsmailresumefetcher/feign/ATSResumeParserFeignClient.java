1a93ea2d0b491d45521d3f4610f2d53fa01e49a6
Anas <lakchouchanas@gmail.com>
2024-02-02 16:21:48 +0100

    /resume/{name} produces JSON data

--- ats-mail-resume-fetcher/src/main/java/com/fst/atsmailresumefetcher/feign/ATSResumeParserFeignClient.java
+++ ats-mail-resume-fetcher/src/main/java/com/fst/atsmailresumefetcher/feign/ATSResumeParserFeignClient.java
@@ -1,6 +1,7 @@
 package com.fst.atsmailresumefetcher.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ATS-RESUME-PARSER")
 public interface ATSResumeParserFeignClient {
 
-    @GetMapping(path = "/resume/{name}/")
+    @GetMapping(path = "/resume/{name}/", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> resumeToJSON(@PathVariable String name);
 }
