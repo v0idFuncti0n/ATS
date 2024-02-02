242b61f11cb84fc56e0ed7dbb2d26e846e953be3
Anas <lakchouchanas@gmail.com>
2024-02-02 13:27:25 +0100

    add ats-discovery-service subproject

---
+++ ats-discovery-service/src/test/java/com/fst/atsdiscoveryservice/AtsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.atsdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
