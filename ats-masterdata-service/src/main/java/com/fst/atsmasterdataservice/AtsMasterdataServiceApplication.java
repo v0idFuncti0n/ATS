92dd95fbf87e11782a8cc171821687af0f51be8c
Anas <lakchouchanas@gmail.com>
2024-02-17 17:40:32 +0100

    add ats-masterdata-service

---
+++ ats-masterdata-service/src/main/java/com/fst/atsmasterdataservice/AtsMasterdataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fst.atsmasterdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsMasterdataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsMasterdataServiceApplication.class, args);
+    }
+
+}
---
+++ ats-masterdata-service/src/test/java/com/fst/atsmasterdataservice/AtsMasterdataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.atsmasterdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsMasterdataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
