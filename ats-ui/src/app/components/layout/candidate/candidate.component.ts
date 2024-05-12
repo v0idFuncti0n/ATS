2687d5e6d6513b8a6e5021dcdadd1f4c7028199f
Anas <lakchouchanas@gmail.com>
2024-05-12 16:16:39 +0100

    get skills and add and delete dynamically from modal

--- ats-ui/src/app/components/layout/candidate/candidate.component.ts
+++ ats-ui/src/app/components/layout/candidate/candidate.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {Config} from "datatables.net";
 import {Candidate} from "../../../models/candidate/Candidate";
 import {CandidateService} from "../../../services/CandidateService";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ModalComponent, ModalConfig} from "../modal/modal.component";
 
 @Component({
@@ -14,8 +14,11 @@ export class CandidateComponent implements OnInit {
   isLoading = true;
   candidates: Candidate[] = [];
   dtOptions: Config = {};
+
   candidateForm: FormGroup;
+
   pdfToDisplayURL: string = "";
+  currentCandidateToVerify: Candidate | undefined;
   candidateModalConfig: ModalConfig = {
     modalTitle: "Verify Candidate",
     closeButtonLabel: "Verify",
@@ -25,7 +28,13 @@ export class CandidateComponent implements OnInit {
   @ViewChild('candidateModal') private candidateModalComponent!: ModalComponent
 
   constructor(private candidateService: CandidateService, private form: FormBuilder) {
-    this.candidateForm = this.form.group([]);
+    this.candidateForm = this.form.group({
+      skills: this.form.array([])
+    })
+  }
+
+  get candidateSkills() {
+    return this.candidateForm.get("skills") as FormArray;
   }
 
   ngOnInit(): void {
@@ -53,6 +62,15 @@ export class CandidateComponent implements OnInit {
   }
 
   async openModalVerifyCandidate(candidate: Candidate) {
+    this.currentCandidateToVerify = candidate;
+
+    this.currentCandidateToVerify.skills.forEach(skill => {
+      const skillForm = this.form.group({
+        skill: [skill.skill,Validators.required]
+      });
+      this.candidateSkills.push(skillForm);
+    })
+
     this.candidateService.getCandidateResumeFile(candidate.id!).subscribe(data => {
       let blob = new Blob([data], {type: 'application/pdf'});
       this.pdfToDisplayURL = window.URL.createObjectURL(blob);
@@ -63,5 +81,19 @@ export class CandidateComponent implements OnInit {
   private verifyCandidate() {
     return true;
   }
+
+
+
+  addSkillFormControl() {
+    const skillForm = this.form.group({
+      skill: ['',Validators.required]
+    });
+    this.candidateSkills.push(skillForm);
+  }
+
+  deleteSkillFormControl(index: number) {
+    this.candidateSkills.removeAt(index);
+  }
+
 }
 
