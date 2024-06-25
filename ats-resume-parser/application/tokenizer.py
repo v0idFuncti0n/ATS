6ffa9eca64b7aab054e4d5a3ebc0cddadefd0f1d
boukhancha <boukhancha.anass@gmail.com>
2024-06-25 22:59:55 +0100

    fix api

--- ats-resume-parser/application/tokenizer.py
+++ ats-resume-parser/application/tokenizer.py
@@ -1,6 +1,5 @@
 import tiktoken
 
-# See: https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
 
 def num_tokens_from_string(string: str, model: str) -> int:
     """Returns the number of tokens in a text string."""
